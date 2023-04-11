007f390deb97667e2f5505a8577264ab21e6f035
Manarbek.2501 <b.manarbek@icloud.com>
2023-04-11 17:34:16 +0600

    Initial Commit

---
+++ BerlinClock/BerlinClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockApp.swift
+//  BerlinClock
+//
+//  Created by Manarbek Bibit on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
